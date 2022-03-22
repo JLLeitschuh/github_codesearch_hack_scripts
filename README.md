2b5873a0220c08f56df39d3fc513e3029bb35353
Jonathan Leitschuh <Jonathan.Leitschuh@gmail.com>
2022-03-22 18:45:28 -0400

    Codesearch working state

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # GitHub Code Search Hack Scripts
 
 A collection of scripts to make working with GitHub Code Search ([cs.github.com](https://cs.github.com)) easier.
+
+## Usage Example
+
+Search for Gradle Build Files that are using `http://` for their `url` in the `repositories` block:
+```shell
+python code_search.py '(" url \"http://" OR "uri(\"http://" OR " url = uri(\"http://") AND ("maven {" OR "maven()" OR "ivy {" OR "ivy()") AND (path:*.gradle OR path:*.gradle.kts)' | jq '.results[] | { repo: .repo_name, path: .path }'
+```
--- code_search.py
+++ code_search.py
@@ -1,10 +1,13 @@
+import json
 from dataclasses import dataclass
 import urllib.parse
 
-import requests as requests
+import requests
+import yaml
 from requests.exceptions import SSLError
 
 from typing import Dict, Callable
+
 
 @dataclass
 class GitHubCodeSearchSite:
@@ -24,10 +27,13 @@ class GitHubCodeSearchSite:
         ))
         return r.json()
 
-    def _make_cs_search(self, search: str, page: int = 0) -> Dict:
+    def make_cs_search(self, search: str, page: int = 0) -> Dict:
         search_encoded = urllib.parse.quote(search)
         url = f'https://cs.github.com/api/search?q={search_encoded}&p={page}'
         return self._make_cs_get(url)
+
+    def make_cs_search_json(self, search: str, page: int = 0) -> str:
+        return json.dumps(self.make_cs_search(search, page), indent=2)
 
     @staticmethod
     def _resilient_request(request_method: Callable[[], requests.Response], retry_count: int = 0):
@@ -37,3 +43,20 @@ class GitHubCodeSearchSite:
             if retry_count < 4:
                 return GitHubCodeSearchSite._resilient_request(request_method, retry_count + 1)
             raise Exception(f'SSL Error') from e
+
+    @staticmethod
+    def create_from_file() -> 'GitHubCodeSearchSite':
+        with open('config.yml') as config_file:
+            config = yaml.safe_load(config_file)
+            github_cs = config['github_cs']
+            return GitHubCodeSearchSite(
+                github_cs['host_blackbird'],
+                github_cs['login_state']
+            )
+
+
+if __name__ == '__main__':
+    import sys
+
+    github_cs = GitHubCodeSearchSite.create_from_file()
+    print(github_cs.make_cs_search_json(' '.join(sys.argv[1:])))
